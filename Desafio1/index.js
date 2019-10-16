bd05f74c8051ba62d9abbe7ed33aecdf6afb0527
Jonatas Freire <jonatasf1997@hotmail.com>
2019-10-16 00:14:40 -0300

    features: Delete and Put

--- Desafio1/index.js
+++ Desafio1/index.js
@@ -3,13 +3,25 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
-const projetos = [];
+const projetos = [{
+  "id": "1",
+  "title": "teste1",
+  "tasks": []
+}, {
+  "id": "2",
+  "title": "teste2",
+  "tasks": []
+}, {
+  "id": "3",
+  "title": "teste3",
+  "tasks": []
+},];
 
 // Adiciona um projeto, com um id e title
 server.post('/projects', (req, res) => {
   const { id, title } = req.body;
   const projeto = {
-    id, 
+    id,
     title,
     tasks: []
   }
@@ -20,6 +32,31 @@ server.post('/projects', (req, res) => {
 
 server.get('/projects', (req, res) => {
   return res.json(projetos);
+});
+
+server.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const { nome } = req.body;
+
+  projetos.map(value => {
+    if (value.id == id) {
+      value.title = nome;
+    }
+  })
+
+  return res.send("Alterado com sucesso");
+});
+
+server.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
+
+  projetos.map((value, index) => {
+    if (value.id == id) {
+      projetos.splice(index, 1);
+    }
+  })
+
+  return res.send("Deletado com sucesso");
 });
 
 server.listen('3333');
